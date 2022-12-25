3122ada9db76b1a0005d84f1d145c1de435aab40
Sayantik Gupta <sayantik03@gmail.com>
2022-12-25 21:08:29 +0530

    First Commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+require('./bootstrap');
+import { createApp } from 'vue';
+import routes from './routes';
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/src/sweetalert2.scss';
+
+const crud = createApp({});
+crud.use(routes).mount('#crud');
+crud.use(VueSweetalert2);
